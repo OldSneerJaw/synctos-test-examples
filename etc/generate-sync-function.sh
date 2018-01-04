bc022a04bf1f3bff01d6c44d4f470ddc52585bdd
Joel <oldsneerjaw@gmail.com>
2018-01-04 13:49:08 -0800

    Update for use with synctos 1.9.4+.

--- etc/generate-sync-function.sh
+++ etc/generate-sync-function.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Determine the current script's directory, so it can execute commands from the root of the project no matter where it was run from
-projectDir="$(dirname "$0")"
+projectDir="$(dirname "$0")/.."
 
 # This is where the generated sync function will be created
 outputDir="$projectDir/build"
@@ -13,4 +13,4 @@ synctosDir="$projectDir/node_modules/synctos"
 mkdir -p "$outputDir"
 
 # Generate the sync function from the document definitions file
-"$synctosDir"/make-sync-function "$projectDir/my-example-doc-definitions.js" "$outputDir/my-example-sync-function.js"
+"$synctosDir"/make-sync-function "$projectDir/src/my-example-doc-definitions.js" "$outputDir/my-example-sync-function.js"
